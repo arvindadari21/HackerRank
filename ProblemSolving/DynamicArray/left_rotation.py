4292e7d3707f61e2769fad66bfee50dd0d9be62d
arvindadari <42448012+arvindadari21@users.noreply.github.com>
2018-12-07 21:47:47 +0530

    Solution for left rotation

---
+++ ProblemSolving/DynamicArray/left_rotation.py
@@ -0,0 +1,54 @@
+"""
+Question:
+
+A left rotation operation on an array of size n shifts each of the array's
+elements 1 unit to the left. For example, if 2 left rotations are performed on
+array [1, 2, 3, 4, 5], then the array would become [3, 4, 5, 1, 2].
+
+Given an array of n integers and a number, d, perform d left rotations on the
+array. Then print the updated array as a single line of space-separated
+integers.
+
+Input Format
+    * The first line contains two space-separated integers denoting the
+      respective values of n(the number of integers) and d(the number of left
+      rotations you must perform).
+    * The second line contains n space-separated integers describing the
+      respective elements of the array's initial state.
+
+Constraints:
+    * 1 <= n <= 10^5
+    * 1 <= d <= n
+    * 1 <= ai <= 10^6
+
+Output Format:
+    Print a single line of n space-separated integers denoting the final state
+    of the array after performing d left rotations.
+
+Sample Input:
+    5 4
+    1 2 3 4 5
+Sample Output:
+    5 1 2 3 4
+
+Explanation:
+    When we perform d=4 left rotations, the array undergoes the following
+    sequence of changes:
+        [1, 2, 3, 4, 5] -> [2, 3, 4, 5, 1] -> [3, 4, 5, 1, 2] -> [4, 5, 1, 2, 3]
+        -> [5, 1, 2, 3, 4]
+    Thus, we print the array's final state as a single line of space-separated
+    values, which is 5 1 2 3 4 .
+"""
+
+# Solution:
+
+if __name__ == '__main__':
+    nd = input("Enter the value of n, d: ").split()
+
+    n = int(nd[0])
+
+    d = int(nd[1])
+
+    a = list(map(int, input("Enter the array elements: ").rstrip().split()))
+
+    print(' '.join(list(map(str, a[d:] + a[:d]))))
