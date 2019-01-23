9477cbf1c1764a933294b5c195200d7b8b9f311d
arvindadari <42448012+arvindadari21@users.noreply.github.com>
2019-01-23 22:51:50 +0530

    Added regex problem solutions

---
+++ Practice/Python/RegularExpressions/validating_roman_numerals.py
@@ -0,0 +1,42 @@
+"""
+Question:
+
+You are given a string, and you have to validate whether it's a valid Roman
+numeral. If it is valid, print True. Otherwise, print False. Try to create a
+regular expression for a valid Roman numeral.
+
+Input Format:
+    A single line of input containing a string of Roman characters.
+
+Output Format:
+    Output a single line containing True or False according to the instructions
+    above.
+
+Constraints:
+    The number will be between 1 and 3999 (both included).
+
+Sample Input:
+    CDXXI
+
+Sample Output:
+    True
+
+References:
+    Regular expressions are a key concept in any programming language. A quick
+    explanation with Python examples is available here. You could also go
+    through the link below to read more about regular expressions in Python.
+
+https://developers.google.com/edu/python/regular-expressions
+"""
+
+# Solution:
+
+import re
+
+th = r'([M]{0,3})'
+hu = '(CM|CD|D?C{0,3})'
+te = '(XL|XC|L?X{0,3})'
+on = '(IV|IX|V?I{0,3})'
+
+regex_pattern = r'{}{}{}{}$'.format(th, hu, te, on)
+print(str(bool(re.match(regex_pattern, input()))))
